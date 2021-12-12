65ed979d1c19342478b8fc1824036b96c0319661
taegorov <taegorov@gmail.com>
2021-12-12 00:59:39 -0800

    adds all auth stuff

--- src/routes/user.js
+++ src/routes/user.js
@@ -17,9 +17,9 @@ router.delete('/user/:id', remove);
 // === === router functions === === //
 async function create(request, response) {
   const userObject = request.body;
-  console.log('request', request.body);
+  console.log('🤖request', request.body);
   const userData = await data.user.create(userObject);
-  console.log('userData', userData);
+  console.log('🥽userData', userData);
 
   response.status(200).send(userData);
 }
--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const cors = require('cors');
 
 const servicesRoutes = require('./routes/services.js')
-const userRoutes = require('./routes/user.js')
+const userRoutes = require('./routes/user.js');
+const authRoutes = require('./routes/auth.js');
 
 const app = express();
 
@@ -13,8 +14,14 @@ app.use(express.json());
 
 console.log('server file, we have arrived')
 
+app.use((req, res, next) => {
+  console.log(req.url, 'ROUTE!!')
+  next()
+})
+
 app.use(servicesRoutes);
 app.use(userRoutes);
+app.use(authRoutes);
 
 // app.post('/user', (req, res) => {
 //   res.send('got to post')
