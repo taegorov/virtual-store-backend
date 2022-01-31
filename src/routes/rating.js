f901dd1222a39cdfa7dd35b492b2a6328e6f4c3b
taegorov <taegorov@gmail.com>
2022-01-30 20:17:23 -0800

    adds rating functionality letting users rate Services between 1 and 5

---
+++ src/routes/rating.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+const { ratings } = require('../models/index.js');
+const authenticateBearer = require('../auth/middleware/bearer')
+
+const router = express.Router();
+
+
+// === === routers === === //
+router.put('/services/:servicesId/rating', authenticateBearer, update);
+
+
+async function update(request, response) {
+  try {
+    const { servicesId } = request.params;
+    const { rating } = request.body;
+    console.log('rating is: ', rating);
+    const ratingData = await ratings.findOne({ where: { id: servicesId, user_id: request.user.id } });
+    console.log('rating data is: ', ratingData);
+
+    let res;
+    if (!ratingData) {
+      // create row here, use .create
+      // the 3 items you want, user_id, service_id, and rating
+      res = await ratings.create({ rating, user_id: request.user.id, service_id: servicesId });
+      console.log('RES FOR RATING .CREATE IS: ', res)
+
+    } else {
+      // rating exists, update existing rating here
+      res = await ratings.update({ rating }, { where: { user_id: request.user.id, service_id: servicesId } });
+      console.log('RES FOR RATING .UPDATE IS: ', res)
+    }
+    // const res = await ratingData.update(rating, { where: { id: ratingData.id } });
+
+    response.status(200).send({ success: res, message: res ? 'Rating Added!' : 'Error Adding Rating!' });
+
+  } catch (error) {
+    console.log(error)
+    response.status(500).send(error);
+  }
+}
+
+
+module.exports = router;
